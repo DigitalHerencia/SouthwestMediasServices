8fa9463b1488efcf1b6b7e8b73b284b0c3b222c1
DigitalHerencia <digitalherencia@outlook.com>
2024-10-14 15:20:57 -0600

    static paths

--- pages/p/[photoId].tsx
+++ pages/p/[photoId].tsx
@@ -29,36 +29,7 @@ const Home: NextPage = ({ currentPhoto }: { currentPhoto: ImageProps }) => {
 };
 
 export default Home;
-
-export const getStaticProps: GetStaticProps = async (context) => {
-    const results = await getResults()
-
-    let reducedResults: ImageProps[] = []
-    let i = 0
-    for (let result of results.resources) {
-        reducedResults.push({
-            id: i,
-            height: result.height,
-            width: result.width,
-            public_id: result.public_id,
-            format: result.format,
-        })
-        i++
-    }
-
-    const currentPhoto = reducedResults.find(
-        (img) => img.id === Number(context.params?.photoId)
-    )
-    if (currentPhoto) {
-        currentPhoto.blurDataUrl = await getBase64ImageUrl(currentPhoto)
-    }
-    return {
-        props: {
-            currentPhoto: currentPhoto,
-        },
-    }
-
-    async function getStaticPaths() {
+    export async function getStaticPaths() {
         const results = await cloudinary.v2.search
             .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
             .sort_by("public_id", "desc")
@@ -80,4 +51,31 @@ export const getStaticProps: GetStaticProps = async (context) => {
             fallback: false,
         }
     }
-}
+    export const getStaticProps: GetStaticProps = async (context) => {
+        const results = await getResults()
+
+        let reducedResults: ImageProps[] = []
+        let i = 0
+        for (let result of results.resources) {
+            reducedResults.push({
+                id: i,
+                height: result.height,
+                width: result.width,
+                public_id: result.public_id,
+                format: result.format,
+            })
+            i++
+        }
+
+        const currentPhoto = reducedResults.find(
+            (img) => img.id === Number(context.params?.photoId)
+        )
+        if (currentPhoto) {
+            currentPhoto.blurDataUrl = await getBase64ImageUrl(currentPhoto)
+        }
+        return {
+            props: {
+                currentPhoto: currentPhoto,
+            },
+        }
+    }
